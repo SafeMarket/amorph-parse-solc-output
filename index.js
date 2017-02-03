9723acc6ea0ab30de064c4988cacf0ef905ae897
Aakil Fernandes <aakilfernandes@gmail.com>
2017-02-03 12:27:03 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const SolcCompilationError = require('./lib/errors/SolcCompilationError')
+const arguguard = require('arguguard')
+const soliditySha3 = require('solidity-sha3')
+const Amorph = require('./lib/Amorph')
+
+module.exports = function amorphParseSolcOuptut(output) {
+  arguguard('amorphParseSolcOuptut', [Object], arguments)
+  if(output.errors) {
+    throw new SolcCompilationError(output.errors[0])
+  }
+  const contracts = {}
+  Object.keys(output.contracts).forEach((contractName) => {
+    _contract = output.contracts[contractName]
+    console.log(_contract)
+    contracts[contractName] = {
+      name: contractName,
+      code: new Amorph(_contract.bytecode, 'hex'),
+      codeHash: new Amorph(soliditySha3.default(`0x${_contract.bytecode}`), 'hex.prefixed'),
+      runcode: new Amorph(_contract.runtimeBytecode, 'hex'),
+      abi: JSON.parse(_contract.interface)
+    }
+  })
+  return contracts
+}
---
+++ test/index.js
@@ -0,0 +1,55 @@
+const amorphParseSolcOuptut = require('../')
+const solc = require('solc')
+const chai = require('chai')
+const SolcCompilationError = require('../lib/errors/SolcCompilationError')
+const Amorph = require('../lib/Amorph')
+
+chai.should()
+
+const GoodContractSol = `
+pragma solidity ^0.4.8;
+
+contract Good {
+  function doThing(){}
+}
+`
+
+const BadContractSol = `
+pragma solidity ^0.4.8;
+contract Bad {} *OH NO!*
+`
+
+const goodOutput = solc.compile(GoodContractSol)
+const badOutput = solc.compile(BadContractSol)
+
+describe('amorphParseSolcOuptut', () => {
+  let parsed
+  it('should throw SolcCompilationError for bad contract', () => {
+    (() => {
+      amorphParseSolcOuptut(badOutput)
+    }).should.throw(SolcCompilationError)
+  })
+  it('should parse goodOutput', () => {
+    parsed = amorphParseSolcOuptut(goodOutput)
+  })
+  describe('parsed', () => {
+    it('should have Good contract', () => {
+      parsed.should.have.keys(['Good'])
+    })
+    it('name should be Good', () => {
+      parsed.Good.name.should.equal('Good')
+    })
+    it('code should be amorph', () => {
+      parsed.Good.code.should.instanceof(Amorph)
+    })
+    it('codeHash should be amorph', () => {
+      parsed.Good.codeHash.should.instanceof(Amorph)
+    })
+    it('runcode should be amorph', () => {
+      parsed.Good.runcode.should.instanceof(Amorph)
+    })
+    it('abi should be a pojo', () => {
+      parsed.Good.abi.should.be.instanceof(Object)
+    })
+  })
+})
